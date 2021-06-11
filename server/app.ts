652fef9533eb5bcf357b2da05e0b7157f26841de
Allison Schambers <aschambers4291@gmail.com>
2021-06-11 13:37:02 -0700

    Add a node server with typescript for learning purposes.

---
+++ server/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { json } from 'body-parser';
+
+const todoRoutes = require('./routes/todos');
+
+const app = express();
+
+app.use(json());
+
+app.use('/todos', todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(3000);
